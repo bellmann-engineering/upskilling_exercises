eb94ce605d8ba132233392dd2321a7003e179f4f
bellmann-engineering <57789413+bellmann-engineering@users.noreply.github.com>
2023-11-24 17:50:43 +0100

    Create isp_dip_violation.cpp

---
+++ isp_dip_violation.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+
+// ISP Violation
+class CSVProcessor {
+public:
+    void process(const std::vector<std::string>& data) {
+        std::cout << "Processing CSV data:" << std::endl;
+        for (const std::string& entry : data) {
+            std::cout << "Record: " << entry << std::endl;
+        }
+    }
+
+    void log(const std::string& message) {
+        std::cout << "Logging: " << message << std::endl;
+    }
+};
+
+class JSONProcessor {
+public:
+    void process(const std::vector<std::string>& data) {
+        std::cout << "Processing JSON data:" << std::endl;
+        for (const std::string& entry : data) {
+            std::cout << "Record: " << entry << std::endl;
+        }
+    }
+
+    void log(const std::string& message) {
+        std::cout << "Logging: " << message << std::endl;
+    }
+};
+
+// DIP Violation
+class DataManager {
+private:
+    CSVProcessor* csvProcessor;
+    JSONProcessor* jsonProcessor;
+
+public:
+    DataManager(CSVProcessor* csvProcessor, JSONProcessor* jsonProcessor)
+        : csvProcessor(csvProcessor), jsonProcessor(jsonProcessor) {}
+
+    void manageCSVData(const std::vector<std::string>& csvData) {
+        std::cout << "Managing CSV data:" << std::endl;
+        csvProcessor->process(csvData);
+        csvProcessor->log("CSV Data processing complete.");
+    }
+
+    void manageJSONData(const std::vector<std::string>& jsonData) {
+        std::cout << "Managing JSON data:" << std::endl;
+        jsonProcessor->process(jsonData);
+        jsonProcessor->log("JSON Data processing complete.");
+    }
+};
+
+int main() {
+    CSVProcessor csvProcessor;
+    JSONProcessor jsonProcessor;
+
+    DataManager dataManager(&csvProcessor, &jsonProcessor);
+
+    std::vector<std::string> csvData = {"1,John,Doe", "2,Jane,Smith", "3,Bob,Johnson"};
+    std::vector<std::string> jsonData = {
+        R"({"id": 1, "first_name": "John", "last_name": "Doe"})",
+        R"({"id": 2, "first_name": "Jane", "last_name": "Smith"})",
+        R"({"id": 3, "first_name": "Bob", "last_name": "Johnson"})"};
+
+    dataManager.manageCSVData(csvData);
+    dataManager.manageJSONData(jsonData);
+
+    return 0;
+}
